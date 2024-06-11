c2dc8f3fe845ab0f06a35cfc93a4a7a50381b15f
Thiago Pinto Braga <thiago.braga@wyntech.inf.br>
2024-06-10 22:54:20 -0300

    some new endpoints

--- main.go
+++ main.go
@@ -11,8 +11,26 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, `{"message": "This is my first API using GoLang"}`)
 }
 
+func statusHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprintf(w, `{"status": "API is running smoothly"}`)
+}
+
+func greetHandler(w http.ResponseWriter, r *http.Request) {
+	name := r.URL.Query().Get("name")
+	if name == "" {
+		name = "Guest"
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprintf(w, `{"message": "Hello %s"}`, name)
+}
+
 func main() {
 	http.HandleFunc("/", helloHandler)
+	http.HandleFunc("/status", statusHandler)
+	http.HandleFunc("/greet", greetHandler)
 
 	// start the server on port 8080
 	fmt.Println("Starting server on port 8080")
