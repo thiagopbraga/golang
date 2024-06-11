23f599bf0786aa70200a85b7e69eee9cd8b7e120
Thiago Pinto Braga <thiago.braga@wyntech.inf.br>
2024-06-10 22:37:23 -0300

    type, interface and methods

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+type Veiculo interface {
+	Andar()
+}
+
+type Carro struct {
+	Modelo string
+	Ano    int
+}
+
+func (c Carro) Andar() {
+	fmt.Println("O carro está andando")
+}
+
+func VemAndarComigo(v Veiculo) {
+	v.Andar()
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+
+	carro1 := Carro{Modelo: "Fusca", Ano: 1970}
+
+	VemAndarComigo(carro1)
 }
